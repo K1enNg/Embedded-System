b3c654ce6060831ed4ec6fecf06ff22db95cd999
Kinkin <hiimkien2702@gmail.com>
2025-11-02 07:09:46 -0500

    initialization and variables assignment

--- getting-started-cpp/01/main.cpp
+++ getting-started-cpp/01/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
 int main() {
+    std::cout <<"Hello World"; // std::cout lives in the iostream library
+
     int x; // definition of variable x
     
+    // x = 21;
 
-    std::cout <<"Hello World"; // std::cout lives in the iostream library
+    int a = 5; //copy-initialization
+    int b ( 4 ); //direct-initialization
+    
+
+    int c { 7 };   // direct-list-initialization (initial value in braces), preferred
+    int d {};      // value-initialization (empty braces)
+
+    [[maybe_unused]] int e = 9; // compiler doesn't warn about unused variables
+    
     return 0;
 }
