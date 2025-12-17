3b0de666b3c7070b43cad4e30af4f6073fab6b9c
Kinkin <hiimkien2702@gmail.com>
2025-12-16 20:13:13 -0500

    variables

--- getting-started-rust/basics/src/main.rs
+++ getting-started-rust/basics/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
+
     loop {
         println!("Please input your attempt.");
 
@@ -37,4 +39,20 @@ fn main() {
         }
     }
     
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}", THREE_HOURS_IN_SECONDS);
+
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value of x is: {}", x);
 }
